7bf47db1077c6b34b238f04faef8a0e4eff7b1f2
Nicolas Petit <nicolaspetitdemeurville@gmail.com>
2025-03-27 03:11:44 -0300

    tweak(script): ItemSO has new string field for items descriptions

--- New Game Plus Interview/Assets/Scripts/ItemSO.cs
+++ New Game Plus Interview/Assets/Scripts/ItemSO.cs
@@ -7,6 +7,9 @@ public class ItemSO : ScriptableObject
     public Sprite itemSprite;
     public int maxStackSize = 1;
     public ItemType itemType;
+    
+    [TextArea]
+    public string itemDescription;
 }
 
 public enum ItemType
